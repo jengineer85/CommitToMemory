24adadc295eb3686b1175230d65cd62a0d3605be
Jennifer G <jengineer85@gmail.com>
2015-04-06 18:02:48 -0400

    The abstract module that all lessons must extend

---
+++ src/com/jgeorgiou/committomemory/ModuleCommitToMemory.java
@@ -0,0 +1,9 @@
+package com.jgeorgiou.committomemory;
+
+public abstract class ModuleCommitToMemory {
+	public abstract int getImageResource();
+	public abstract int getTextResource();
+	public abstract int getAudioResource();
+	public abstract int getAudioImageResource();
+
+}
